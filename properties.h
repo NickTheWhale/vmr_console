c2838cefb2338331c52e956c4ba5991f7bd0b7b7
NickTheWhale <51139062+NickTheWhale@users.noreply.github.com>
2022-05-06 23:55:21 -0500

    idek

--- properties.h
+++ properties.h
@@ -5,10 +5,31 @@
 /**                 Active routing             **/
 /************************************************/
 
+/*
+	array of routing information from routing.txt in the form:
+	"Strip[<index>].<parameter>:<control index>",
+	"Strip[<index>].<parameter>:<control index>"
+*/
 char routing[128][64]{};
 
+
+/*
+	array of parameter names derived from routing[][] in the form:
+	"Strip[<index>].<parameter>:",
+	"Strip[<index>].<parameter>"
+*/
 char paramNames[128][48]{};
 
+
+/*
+	array of control index derived from routing[][] in the form:
+	"<control index>",
+	"<control index>"
+*/
 char control[128][8]{};
 
+
+/*
+	parameter values updated by isDirty()
+*/
 float paramStates[128]{};
